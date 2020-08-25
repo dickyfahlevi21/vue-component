7b5299f7b4630e0fa45840c7602278bbbc5ae3a0
dickyfahlevi21 <dicky.fahlevi21@gmail.com>
2020-08-25 09:31:34 +0700

    fixing selected item

--- src/components/ListRendering.vue
+++ src/components/ListRendering.vue
@@ -6,7 +6,7 @@
                 <div class="px-6 py-4">
                     <div class="font-bold text-xl mb-2">{{ json.title }}</div>
                     <p class="text-gray-700 text-base">{{ json.id }}</p>
-                    <input class="mr-2 leading-tight" type="checkbox" @input="$emit('click', $event.target.value)"> check me for select
+                    <input class="mr-2 leading-tight" type="checkbox" @input="$emit('click', data)"> check me for select
                 </div>
             </div>
         </div>
